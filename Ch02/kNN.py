beae0f01a0efbf9214b9a9032a350b074f95f729
leon-zheng <misterdayang@gmail.com>
2018-09-11 21:08:11 +0800

    changed import alias

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -10,14 +10,15 @@ Input:      inX: vector to compare to existing dataset (1xN)
 Output:     the most popular class label
 
 @author: pbharrin
+@co-author: leon-zheng
 '''
-from numpy import *
+import numpy as np
 import operator
 from os import listdir
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    diffMat = np.tile(inX, (dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
@@ -26,23 +27,18 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-        
-        #if classCount.has_key(voteIlabel):
-        #    classCount[voteIlabel] += 1
-        #else:
-        #    classCount[voteIlabel] = 1
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
 def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    group = np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
     return group, labels
 
 def file2matrix(filename):
     fr = open(filename)
     numberOfLines = len(fr.readlines())         #get the number of lines in the file
-    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    returnMat = np.zeros((numberOfLines,3))        #prepare matrix to return
     classLabelVector = []                       #prepare labels return   
     fr = open(filename)
     index = 0
@@ -58,10 +54,10 @@ def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
+    normDataSet = np.zeros(shape(dataSet))
     m = dataSet.shape[0]
-    normDataSet = dataSet - tile(minVals, (m,1))
-    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    normDataSet = dataSet - np.tile(minVals, (m,1))
+    normDataSet = normDataSet/np.tile(ranges, (m,1))   #element wise divide
     return normDataSet, ranges, minVals
    
 def datingClassTest():
@@ -79,7 +75,7 @@ def datingClassTest():
     print errorCount
     
 def img2vector(filename):
-    returnVect = zeros((1,1024))
+    returnVect = np.zeros((1,1024))
     fr = open(filename)
     for i in range(32):
         lineStr = fr.readline()
@@ -91,7 +87,7 @@ def handwritingClassTest():
     hwLabels = []
     trainingFileList = listdir('trainingDigits')           #load the training set
     m = len(trainingFileList)
-    trainingMat = zeros((m,1024))
+    trainingMat = np.zeros((m,1024))
     for i in range(m):
         fileNameStr = trainingFileList[i]
         fileStr = fileNameStr.split('.')[0]     #take off .txt
