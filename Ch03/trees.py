bf5b6e5b9ab94ca10bf02b3ecba3e7a763d275c3
leon-zheng <misterdayang@gmail.com>
2018-09-13 16:42:38 +0800

    use dic.get

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -13,48 +13,46 @@ def createDataSet():
                [0, 1, 'no'],
                [0, 1, 'no']]
     labels = ['no surfacing','flippers']
-    #change to discrete values
     return dataSet, labels
 
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
-    for featVec in dataSet: #the the number of unique elements and their occurance
-        currentLabel = featVec[-1]
-        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
-        labelCounts[currentLabel] += 1
+    for featVec in dataSet:
+        label = featVec[-1]
+        labelCounts[label] = labelCounts.get(label, 0) + 1
     shannonEnt = 0.0
     for key in labelCounts:
         prob = float(labelCounts[key])/numEntries
-        shannonEnt -= prob * log(prob,2) #log base 2
+        shannonEnt -= prob * log(prob,2)
     return shannonEnt
-    
+
 def splitDataSet(dataSet, axis, value):
     retDataSet = []
     for featVec in dataSet:
         if featVec[axis] == value:
-            reducedFeatVec = featVec[:axis]     #chop out axis used for splitting
+            reducedFeatVec = featVec[:axis]
             reducedFeatVec.extend(featVec[axis+1:])
             retDataSet.append(reducedFeatVec)
     return retDataSet
-    
+
 def chooseBestFeatureToSplit(dataSet):
-    numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
+    numFeatures = len(dataSet[0]) - 1
     baseEntropy = calcShannonEnt(dataSet)
     bestInfoGain = 0.0; bestFeature = -1
-    for i in range(numFeatures):        #iterate over all the features
-        featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
-        uniqueVals = set(featList)       #get a set of unique values
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
         newEntropy = 0.0
         for value in uniqueVals:
             subDataSet = splitDataSet(dataSet, i, value)
             prob = len(subDataSet)/float(len(dataSet))
             newEntropy += prob * calcShannonEnt(subDataSet)     
-        infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
-        if (infoGain > bestInfoGain):       #compare this to the best gain so far
-            bestInfoGain = infoGain         #if better than current best, set to best
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
             bestFeature = i
-    return bestFeature                      #returns an integer
+    return bestFeature
 
 def majorityCnt(classList):
     classCount={}
@@ -67,8 +65,8 @@ def majorityCnt(classList):
 def createTree(dataSet,labels):
     classList = [example[-1] for example in dataSet]
     if classList.count(classList[0]) == len(classList): 
-        return classList[0]#stop splitting when all of the classes are equal
-    if len(dataSet[0]) == 1: #stop splitting when there are no more features in dataSet
+        return classList[0]
+    if len(dataSet[0]) == 1:
         return majorityCnt(classList)
     bestFeat = chooseBestFeatureToSplit(dataSet)
     bestFeatLabel = labels[bestFeat]
@@ -77,10 +75,10 @@ def createTree(dataSet,labels):
     featValues = [example[bestFeat] for example in dataSet]
     uniqueVals = set(featValues)
     for value in uniqueVals:
-        subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
+        subLabels = labels[:]
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels)
-    return myTree                            
-    
+    return myTree
+
 def classify(inputTree,featLabels,testVec):
     firstStr = inputTree.keys()[0]
     secondDict = inputTree[firstStr]
@@ -97,9 +95,8 @@ def storeTree(inputTree,filename):
     fw = open(filename,'w')
     pickle.dump(inputTree,fw)
     fw.close()
-    
+
 def grabTree(filename):
     import pickle
     fr = open(filename)
     return pickle.load(fr)
-    
