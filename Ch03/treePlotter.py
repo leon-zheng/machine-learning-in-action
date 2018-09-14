2f940dde2798d99b09010f832332bc861f9c4ca8
leon-zheng <misterdayang@gmail.com>
2018-09-14 22:03:17 +0800

    reorganize

--- Ch03/treePlotter.py
+++ Ch03/treePlotter.py
@@ -2,6 +2,7 @@
 Created on Oct 14, 2010
 
 @author: Peter Harrington
+modified on Sep 14, 2018 by leon-zheng
 '''
 import matplotlib.pyplot as plt
 
@@ -14,7 +15,8 @@ def getNumLeafs(myTree):
     firstStr = myTree.keys()[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
-        if type(secondDict[key]).__name__=='dict':#test to see if the nodes are dictonaires, if not they are leaf nodes
+        #test to see if the nodes are dictonaires, if not they are leaf nodes
+        if type(secondDict[key]).__name__=='dict':
             numLeafs += getNumLeafs(secondDict[key])
         else:   numLeafs +=1
     return numLeafs
@@ -24,7 +26,8 @@ def getTreeDepth(myTree):
     firstStr = myTree.keys()[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
-        if type(secondDict[key]).__name__=='dict':#test to see if the nodes are dictonaires, if not they are leaf nodes
+        #test to see if the nodes are dictonaires, if not they are leaf nodes
+        if type(secondDict[key]).__name__=='dict':
             thisDepth = 1 + getTreeDepth(secondDict[key])
         else:   thisDepth = 1
         if thisDepth > maxDepth: maxDepth = thisDepth
@@ -38,23 +41,32 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
 def plotMidText(cntrPt, parentPt, txtString):
     xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
     yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
-    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+    createPlot.ax1.text(xMid, yMid, txtString, \
+                        va="center", ha="center", rotation=30)
 
-def plotTree(myTree, parentPt, nodeTxt):#if the first key tells you what feat was split on
+def plotTree(myTree, parentPt, nodeTxt):
+    '''
+    if the first key tells you what feat was split on
+    '''
+    
     numLeafs = getNumLeafs(myTree)  #this determines the x width of this tree
     depth = getTreeDepth(myTree)
-    firstStr = myTree.keys()[0]     #the text label for this node should be this
-    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)
+    firstStr = myTree.keys()[0]     #text label for this node should be this
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/ \
+              2.0/plotTree.totalW, plotTree.yOff)
     plotMidText(cntrPt, parentPt, nodeTxt)
     plotNode(firstStr, cntrPt, parentPt, decisionNode)
     secondDict = myTree[firstStr]
     plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
     for key in secondDict.keys():
-        if type(secondDict[key]).__name__=='dict':#test to see if the nodes are dictonaires, if not they are leaf nodes   
-            plotTree(secondDict[key],cntrPt,str(key))        #recursion
-        else:   #it's a leaf node print the leaf node
+        #test to see if the nodes are dictonaires, if not they are leaf nodes
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key],cntrPt,str(key))
+        #it's a leaf node print the leaf node
+        else:
             plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
-            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), \
+                     cntrPt, leafNode)
             plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
     plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
 
@@ -71,7 +83,9 @@ def createPlot(inTree):
     plt.show()
 
 def retrieveTree(i):
-    listOfTrees =[{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
-                  {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
+    listOfTrees = [
+    {'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+    {'no surfacing': \
+     {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
                   ]
     return listOfTrees[i]
